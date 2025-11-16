6666199a735b38fae29c116323efb49e17c56232
EvanSigma <evannolan55@gmail.com>
2025-11-16 12:40:07 +0700

    1

---
+++ griya-mdp/src/app/lokasi-perumahan/housing.model.ts
@@ -0,0 +1,16 @@
+
+export interface Housing {
+  id: number;
+  title: string;
+  location: string;
+  price: number;
+  bedrooms: number;
+  bathrooms: number;
+  area: number;
+  image: string;
+  rating: number;
+  status: string;
+  type?: string;
+  description?: string;
+  postedDays?: number;
+}
